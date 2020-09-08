47509dcd4a32a3388070f0587bcb0998b82a91dd
Ozzy Osborne <ozzy@ca.ibm.com>
2020-09-08 14:25:54 -0400

    Move to UBI

--- map-wlpcfg/startup.sh
+++ map-wlpcfg/startup.sh
@@ -83,8 +83,8 @@ if [ "$GAMEON_MODE" == "development" ]; then
   ensure_exists map_repository/_design/site --data-binary @/opt/site.json
 
   echo "** Checking firstroom"
-  sed "s/game-on.org/${SYSTEM_ID}/g" /opt/firstRoom.json > /opt/firstRoom.withid.json
-  ensure_exists map_repository/firstroom --data-binary @/opt/firstRoom.withid.json
+  sed "s/game-on.org/${SYSTEM_ID}/g" /opt/firstRoom.json > /tmp/firstRoom.withid.json
+  ensure_exists map_repository/firstroom --data-binary @/tmp/firstRoom.withid.json
 else
   activeUrl=${AUTH_URL}
   assert_exists map_repository
